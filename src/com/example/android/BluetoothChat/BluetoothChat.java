20624ff4fe7392877c17d134f85c2edb468ff72a
MiichaelD <michael.duarte@hotmail.com>
2014-10-03 20:05:08 -0400

    Complete BluetoothChat on different screen orientations
    Finished implementing Singleton to BluetoothChatService and
    BluetoothHandler also added sending functionality to "enter" key

--- src/com/example/android/BluetoothChat/BluetoothChat.java
+++ src/com/example/android/BluetoothChat/BluetoothChat.java
@@ -247,14 +247,26 @@ public class BluetoothChat extends Activity {
     // The action listener for the EditText widget, to listen for the return key
     private TextView.OnEditorActionListener mWriteListener =
         new TextView.OnEditorActionListener() {
+    	@Override
         public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
             // If the action is a key-up event on the return key, send the message
-            if (actionId == EditorInfo.IME_ACTION_DONE && event.getAction() == KeyEvent.ACTION_UP) {
-                String message = view.getText().toString();
-                sendMessage(message);
-            }
-            if(Constants.D) Log.i(Constants.TAG, "END onEditorAction");
-            return true;
+        	try{
+	            if(actionId == EditorInfo.IME_ACTION_SEND) {
+	            	/*
+	            	InputMethodManager imm = (InputMethodManager)getSystemService(
+	            		      Context.INPUT_METHOD_SERVICE);
+	            		imm.hideSoftInputFromWindow(mOutEditText.getWindowToken(), 0);
+	            		*/
+	                String message = view.getText().toString();
+	                sendMessage(message);
+	            }
+	            if(Constants.D) Log.i(Constants.TAG, "END onEditorAction");
+	            return true;
+        	}catch(Exception e){
+        		if(Constants.D) Log.e(Constants.TAG, "END onEditorAction");
+        		e.printStackTrace();
+        	}
+    		return false;
         }
     };
 
